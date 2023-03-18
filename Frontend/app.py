698332b06e5c03db08547085f4c4b06adb1aba47
Neha Goel <55681416+neha19066@users.noreply.github.com>
2023-03-18 16:28:25 +0530

    Ingredients going to backend.

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,23 +1,10 @@
 from flask import Flask, render_template, request
 import pickle
 
+list_ingredients=[]
+
 app = Flask(__name__)
 
-# define the list of ingredients
-# categories = [
-#     {'name': 'Meat', 'ingredients': [
-#         'Beef', 'Chicken', 'Pork', 'Lamb', 'Turkey', 'Salmon', 'Tuna', 'Shrimp', 'Egg', 'Cheese']},
-#     {'name': 'Vegetables', 'ingredients': [
-#         'Carrots', 'Broccoli', 'Spinach', "Tomato", "Onion", "Butter Lettuce", "Cucumber",
-#         "Celery", "Potato", "Garlic", "Mushroom", "Pepper", "Zucchini", "Cauliflower", "Eggplant",
-#         "Sweet Potato", "Green Beans", "Asparagus", "Avocado", "Lettuce", "Spinach", "Kale", "Cabbage",
-#         "Broccoli", "Brussels Sprouts", "Cucumber", "Tomato", "Corn", "Peas", "Bell Pepper", "Carrot",
-#     ]},
-#     {'name': 'Fruits', 'ingredients': [
-#         'Apples', 'Oranges', 'Bananas', 'Pears', 'Grapes', 'Strawberries', 'Blueberries', 'Raspberries', ]},
-#     {'name': 'Spices', 'ingredients': [
-#         'Salt', 'Pepper', 'Cumin', "Star Anise", "Turmeric", "Cardamom"]}
-# ]
 with open('../Backend/Saved/ingredients_supercook_for_flask', 'rb') as f:
     categories = pickle.load(f)
 
@@ -33,10 +20,15 @@ def home():
 
 @app.route('/suggest-recipe', methods=['POST'])
 def suggest_recipe():
-    selected_ingredients = request.form.getlist('ingredients')
-    # logic to suggest recipe based on selected ingredients goes here
-    # return the recipe suggestion to the user
-    return f'The recipe suggestion based on your selected ingredients {selected_ingredients} is: <insert recipe here>'
+    print('hi')
+    selected_ingredients = request.form.get('param')
+    if(request.form.get('type')=='a'):
+        list_ingredients.append(selected_ingredients)
+    else:
+        list_ingredients.remove(selected_ingredients)
+    print(list_ingredients)
+    print('hi')
+    return ('success', True)
 
 
 if __name__ == '__main__':
